879110f86b76b498e3a334f6a32adb8497873d07
Hibi_10000 <Hibi-10000@outlook.jp>
2025-03-22 09:45:33 +0900

    remove return boolean

--- tasks/engines/fontforge.js
+++ tasks/engines/fontforge.js
@@ -34,11 +34,13 @@ export default (o, allDone) => {
 
 	const proc = exec(args, { maxBuffer: o.execMaxBuffer }, (err, out, code) => {
 		if (err instanceof Error && err.code === 127) {
-			return fontforgeNotFound();
+			fontforgeNotFound();
+			return;
 		}
 		else if (err) {
 			if (err instanceof Error) {
-				return error(err.message);
+				error(err.message);
+				return;
 			}
 
 			// Skip some fontforge output such as copyrights. Show warnings only when no font files was created
@@ -59,7 +61,8 @@ export default (o, allDone) => {
 			});
 
 			if (warn.length) {
-				return error(warn.join('\n'));
+				error(warn.join('\n'));
+				return;
 			}
 		}
 
@@ -73,12 +76,13 @@ export default (o, allDone) => {
 		}
 		catch (e) {
 			logger.verbose('Webfont did not receive a proper JSON result from Python script: ' + e);
-			return error(
+			error(
 				'Something went wrong when running fontforge. Probably fontforge wasn’t installed correctly or one of your SVGs is too complicated for fontforge.\n\n' +
 				'1. Try to run Grunt in verbose mode: ' + chalk.bold('grunt --verbose webfont') + ' and see what fontforge says. Then search GitHub issues for the solution: ' + chalk.underline('https://github.com/sapegin/grunt-webfont/issues') + '.\n\n' +
 				'2. Try to use “node” engine instead of “fontforge”: ' + chalk.underline('https://github.com/sapegin/grunt-webfont#engine') + '\n\n' +
 				'3. To find “bad” icon try to remove SVGs one by one until error disappears. Then try to simplify this SVG in Sketch, Illustrator, etc.\n\n'
 			);
+			return;
 		}
 
 		allDone({
@@ -109,7 +113,6 @@ export default (o, allDone) => {
 					chalk.underline('https://github.com/sapegin/grunt-webfont#execMaxBuffer') + ". "
 			);
 		}
-		return true;
 	});
 
 	const params = _.extend(o, {
@@ -121,7 +124,6 @@ export default (o, allDone) => {
 	function error() {
 		logger.error.apply(null, arguments);
 		allDone(false);
-		return false;
 	}
 
 	const fontforgeNotFound = () => {

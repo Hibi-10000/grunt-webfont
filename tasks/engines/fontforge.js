893fa68b96096a805a9d7321316345e3ac89dfa7
Hibi_10000 <Hibi-10000@outlook.jp>
2025-04-29 00:39:25 +0900

    revert back to removing possibly unreachable statement

--- tasks/engines/fontforge.js
+++ tasks/engines/fontforge.js
@@ -14,6 +14,7 @@ import temp from 'temp';
 import chalk from 'chalk';
 import _ from 'lodash';
 import winston from 'winston';
+import * as wf from '../util/util.js';
 
 /** @type {(o: OptionsInternal, allDone: (result: { fontName: string } | false) => void) => void} */
 export default (o, allDone) => {
@@ -75,6 +76,29 @@ export default (o, allDone) => {
 			}
 			if (err instanceof Error) {
 				error(err.message);
+				return;
+			}
+			logger.error(`probably an impossible error`);
+
+			// Skip some fontforge output such as copyrights. Show warnings only when no font files was created
+			// or in verbose mode.
+			const success = !!wf.generatedFontFiles(o);
+			const notError = /(Copyright|License |with many parts BSD |Executable based on sources from|Library based on sources from|Based on source from git)/;
+			//const version = /(Executable based on sources from|Library based on sources from)/;
+			const lines = err.split('\n');
+
+			const warn = [];
+			lines.forEach((line) => {
+				if (!line.match(notError) && !success) {
+					warn.push(line);
+				}
+				else {
+					logger.verbose(chalk.grey('fontforge: ') + line);
+				}
+			});
+
+			if (warn.length) {
+				error(warn.join('\n'));
 				return;
 			}
 			error(`impossible error: ${err}`);

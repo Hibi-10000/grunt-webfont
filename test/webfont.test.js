a9178cc886094dbdbe6a80a3ece5d1150785d195
Hibi_10000 <Hibi-10000@outlook.jp>
2025-05-07 18:39:57 +0900

    add test with node:test

---
+++ test/webfont.test.js
@@ -0,0 +1,357 @@
+import test from "node:test";
+
+import fs from "node:fs";
+import path from "node:path";
+import { globSync } from "glob";
+
+import { webfont } from "../tasks/webfont.js";
+import { webfont as webfontTests } from "./webfont_test.js";
+
+fs.rmSync('test/tmp', { recursive: true, force: true });
+
+/** @type {Configs} */
+const configs = {
+	test1: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/test1',
+		options: {
+			hashes: false
+		}
+	},
+	test2: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/test2/fonts',
+		destCss: 'test/tmp/test2',
+		options: {
+			font: 'myfont',
+			types: 'woff,svg',
+			syntax: 'bootstrap'
+		}
+	},
+	embed: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/embed',
+		options: {
+			hashes: false,
+			embed: true
+		}
+	},
+	embed_woff: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/embed_woff',
+		options: {
+			types: 'woff',
+			hashes: false,
+			embed: true
+		}
+	},
+	embed_ttf: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/embed_ttf',
+		options: {
+			types: 'ttf',
+			hashes: false,
+			embed: 'ttf'
+		}
+	},
+	embed_ttf_woff: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/embed_ttf_woff',
+		options: {
+			types: 'ttf,woff',
+			hashes: false,
+			embed: 'ttf,woff'
+		}
+	},
+	one: {
+		src: 'test/src_one/*.svg',
+		dest: 'test/tmp/one',
+		options: {
+			hashes: false
+		}
+	},
+	template: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/template',
+		options: {
+			template: 'test/templates/template.css'
+		}
+	},
+	html_template: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/html_template',
+		options: {
+			htmlDemoTemplate: 'test/templates/template.html'
+		}
+	},
+	html_filename: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/html_filename',
+		options: {
+			htmlDemoFilename: 'index'
+		}
+	},
+	relative_path: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/relative_path',
+		options: {
+			relativeFontPath: '../iamrelative',
+			hashes: false
+		}
+	},
+	spaces: {
+		src: 'test/src_space/*.svg',
+		dest: 'test/tmp/spaces'
+	},
+	disable_demo: {
+		src: 'test/src_one/*.svg',
+		dest: 'test/tmp/disable_demo',
+		options: {
+			htmlDemo: false
+		}
+	},
+	parent_source: {
+		src: '../grunt-webfont/test/src/*.svg',
+		dest: 'test/tmp/parent_source',
+		options: {
+			hashes: false
+		}
+	},
+	// #167: Ligatures with hyphen don’t work
+	ligatures: {
+		src: 'test/src_ligatures/*.svg',
+		dest: 'test/tmp/ligatures',
+		options: {
+			hashes: false,
+			ligatures: true
+		}
+	},
+	duplicate_names: {
+		src: '../grunt-webfont/test/src_duplicate_names/**/*.svg',
+		dest: 'test/tmp/duplicate_names',
+		options: {
+			hashes: false,
+			rename: (/** @type {string} */name) => {
+				return [path.basename(path.dirname(name)), path.basename(name)].join('-');
+			}
+		}
+	},
+	order: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/order',
+		options: {
+			types: 'woff,svg',
+			order: 'svg,woff',
+			hashes: false
+		}
+	},
+	template_options: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/template_options',
+		options: {
+			hashes: false,
+			syntax: 'bem',
+			//stylesheet: 'less',
+			templateOptions: {
+				baseClass: 'glyph-icon',
+				classPrefix: 'glyph_'
+			}
+		}
+	},
+	node: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/node',
+		options: {
+			hashes: false,
+			engine: 'node'
+		}
+	},
+	ie7: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/ie7',
+		options: {
+			hashes: false,
+			ie7: true,
+			syntax: 'bem'
+		}
+	},
+	ie7_bootstrap: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/ie7_bootstrap',
+		options: {
+			hashes: false,
+			ie7: true,
+			syntax: 'bootstrap'
+		}
+	},
+	optimize_enabled: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/optimize_enabled',
+		options: {
+			engine: 'node',
+			types: 'svg',
+			autoHint: false,
+			optimize: true
+		}
+	},
+	optimize_disabled: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/optimize_disabled',
+		options: {
+			engine: 'node',
+			types: 'svg',
+			autoHint: false,
+			optimize: false
+		}
+	},
+	codepoints: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/codepoints',
+		options: {
+			hashes: false,
+			startCodepoint: 0x41,
+			codepoints: {
+				single: 0x43
+			}
+		}
+	},
+	camel: {
+		src: 'test/camel/*.svg',
+		dest: 'test/tmp/camel',
+		options: {
+			hashes: false
+		}
+	},
+	folders: {
+		src: 'test/src_folders/**/*.svg',
+		dest: 'test/tmp/folders',
+		options: {
+			hashes: false
+		}
+	},
+	woff2: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/woff2',
+		options: {
+			types: 'woff2,woff'
+		}
+	},
+	woff2_node: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/woff2_node',
+		options: {
+			types: 'woff2,woff',
+			engine: 'node'
+		}
+	},
+	target_overrides: {
+		src: 'test/src/*.svg',
+		options: {
+			dest: 'test/tmp/target_overrides_icons',
+			destCss: 'test/tmp/target_overrides_css',
+		}
+	},
+	font_family_name: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/font_family_name',
+		options: {
+			fontFamilyName: 'customName',
+			types: 'ttf',
+		}
+	},
+	custom_output: {
+		src: 'test/src/*.svg',
+		options: {
+			dest: 'test/tmp/custom_output_icons',
+			destCss: 'test/tmp/custom_output_css',
+			customOutputs: [{
+				template: 'test/templates/custom.js',
+				dest: 'test/tmp/custom_output/test-icon-config.js'
+			}, {
+				template: 'test/templates/custom.json',
+				dest: 'test/tmp/custom_output'
+			}, {
+				template: 'test/templates/context-test.html',
+				dest: 'test/tmp/custom_output',
+				context: {
+					testHeading: 'Hello, world!'
+				}
+			}]
+		}
+	},
+	enabled_template_variables: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/enabled_template_variables',
+		options: {
+			relativeFontPath: '../iamrelative',
+			//fontPathVariables: true,
+			//stylesheets: ['css']
+		}
+	},
+	filename_length: {
+		src: 'test/src_filename_length/*.svg',
+		dest: 'test/tmp/filename_length',
+		options: {
+			autoHint: false,
+			engine: 'node',
+			hashes: false,
+			types: 'woff'
+		}
+	},
+};
+
+const notImpl = () => {
+	throw new Error("Function not implemented.");
+};
+
+/** @type {(t: import('node:test').TestContext, done: (value?: never) => void) => import('nodeunit').Test} */
+const nodeUnit_Test = (t, done) => ({
+	ok: t.assert.ok,
+	done: done,
+	expect: notImpl,
+	fail: t.assert.fail,
+	assert: notImpl,
+	equal: t.assert.equal,
+	notEqual: notImpl,
+	deepEqual: notImpl,
+	notDeepEqual: notImpl,
+	strictEqual: notImpl,
+	notStrictEqual: notImpl,
+	throws: notImpl,
+	doesNotThrow: notImpl,
+	ifError: notImpl,
+	equals: t.assert.equal,
+	same: notImpl, //deepEqual
+});
+
+await test('webfont', { concurrency: true }, async (t) => {
+	const cases = [];
+	for (const key in configs) {
+		const config = configs[key];
+		const options = config.options ?? {};
+		const filesSrc = globSync(config.src, { posix: true });
+		cases.push(
+			webfont("webfont", key, filesSrc, config, options)
+		);
+	}
+	await Promise.allSettled(cases);
+	/** @type {Promise<void>[]} */
+	const tests = [];
+	///** @type {((value?: never) => void)[]} */
+	//const resolves = [];
+	for (const key in webfontTests) {
+		tests.push(t.test(key, async (t) => {
+			//await new Promise((resolve) => {
+			//	resolves.push(resolve);
+			//});
+			await new Promise(async (resolve) =>
+				webfontTests[key](nodeUnit_Test(t, resolve))
+			);
+		}));
+	}
+	//await Promise.allSettled(cases);
+	//resolves.forEach((resolve) => resolve());
+	await Promise.allSettled(tests);
+});
+
+fs.rmSync('test/tmp', { recursive: true, force: true });

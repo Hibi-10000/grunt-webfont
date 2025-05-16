b42d1b46424673dc9d4abbde444fd477223ca436
Hibi_10000 <Hibi-10000@outlook.jp>
2025-05-16 23:25:25 +0900

    sync changes grunt config with node test

--- test/webfont.test.js
+++ test/webfont.test.js
@@ -81,6 +81,21 @@ const configs = {
 			template: 'test/templates/template.css',
 		},
 	},
+	template_scss: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/template_scss',
+		options: {
+			stylesheet: 'scss',
+			template: 'test/templates/template.scss'
+		}
+	},
+	template_sass: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/template_sass',
+		options: {
+			template: 'test/templates/template.sass'
+		}
+	},
 	html_template: {
 		src: 'test/src/*.svg',
 		dest: 'test/tmp/html_template',
@@ -103,6 +118,44 @@ const configs = {
 			hashes: false,
 		},
 	},
+	sass: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/sass',
+		options: {
+			stylesheet: 'sass'
+		}
+	},
+	less: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/less',
+		options: {
+			stylesheet: 'less'
+		}
+	},
+	css_plus_scss: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/sass',
+		destCss: 'test/tmp/css',
+		destScss: 'test/tmp/scss',
+		options: {
+			stylesheets: ['css', 'scss']
+		}
+	},
+	stylus_bem: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/stylus_bem',
+		options: {
+			stylesheet: 'styl'
+		}
+	},
+	stylus_bootstrap: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/stylus_bootstrap',
+		options: {
+			stylesheet: 'styl',
+			syntax: 'bootstrap'
+		}
+	},
 	spaces: {
 		src: 'test/src_space/*.svg',
 		dest: 'test/tmp/spaces'
@@ -113,6 +166,15 @@ const configs = {
 		options: {
 			htmlDemo: false,
 		},
+	},
+	non_css_demo: {
+		src: 'test/src/*.svg',
+		dest: 'test/tmp/non_css_demo',
+		options: {
+			stylesheet: 'less',
+			relativeFontPath: '../iamrelative',
+			htmlDemo: true
+		}
 	},
 	parent_source: {
 		src: '../grunt-webfont/test/src/*.svg',
@@ -155,7 +217,7 @@ const configs = {
 		options: {
 			hashes: false,
 			syntax: 'bem',
-			//stylesheet: 'less',
+			stylesheet: 'less',
 			templateOptions: {
 				baseClass: 'glyph-icon',
 				classPrefix: 'glyph_',
@@ -288,8 +350,8 @@ const configs = {
 		dest: 'test/tmp/enabled_template_variables',
 		options: {
 			relativeFontPath: '../iamrelative',
-			//fontPathVariables: true,
-			//stylesheets: ['css'],
+			fontPathVariables: true,
+			stylesheets: ['css', 'scss', 'less'],
 		},
 	},
 	filename_length: {

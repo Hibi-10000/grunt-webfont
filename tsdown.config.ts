d0548c8e8bb651062f183e86e1a170512005f867
Hibi_10000 <Hibi-10000@outlook.jp>
2025-09-13 10:01:51 +0900

    copy global.d.ts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-    entry: "tasks/webfont.ts",
+	entry: "tasks/webfont.ts",
 	unbundle: true,
-    sourcemap: true,
-    format: "esm",
+	sourcemap: true,
+	format: "esm",
 	cjsDefault: false,
 	dts: {
 		oxc: true,
 	},
+	copy: [
+		"tasks/global.d.ts",
+	],
 	skipNodeModulesBundle: true,
 	platform: "node",
 });

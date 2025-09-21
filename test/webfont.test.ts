0cfa85cbea19c0524a358b55c2e371065538367a
Hibi_10000 <Hibi-10000@outlook.jp>
2025-09-21 23:47:46 +0900

    fix IO Error on Windows

--- test/webfont.test.ts
+++ test/webfont.test.ts
@@ -392,7 +392,7 @@ const nodeUnit_Test = (t: TestContext, done: (value?: never) => void): Test => (
 	same: t.assert.deepEqual,
 });
 
-await test('webfont', { concurrency: true }, async (t) => {
+await test('webfont', { concurrency: process.platform !== 'win32' }, async (t) => {
 	const tests: Promise<void>[] = [];
 	for (const key in configs) {
 		const config = configs[key];

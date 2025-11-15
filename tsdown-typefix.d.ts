bd5c993d0874afff85c3043737470c16f4641765
Hibi_10000 <Hibi-10000@outlook.jp>
2025-11-15 23:16:42 +0900

    fix typecheck error

--- tsdown-typefix.d.ts
+++ tsdown-typefix.d.ts
@@ -2,14 +2,18 @@ declare module "@arethetypeswrong/core" {
 	export type CheckPackageOptions = any;
 }
 
+declare module "@vitejs/devtools/cli-commands" {
+	export type StartOptions = any;
+}
+
+declare module "pkg-types" {
+	export type PackageJson = any;
+}
+
 declare module "publint" {
 	export type Options = any;
 }
 
 declare module "unplugin-unused" {
 	export type Options = any;
-}
-
-declare module "pkg-types" {
-	export type PackageJson = any;
 }
